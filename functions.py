2bc50bfa2a532d85103f03df8478d51c7c4f6f75
highonchai <connor.high@gmail.com>
2020-11-06 17:05:43 -0500

    Adding header info to functions file + my connection file

---
+++ functions.py
@@ -0,0 +1,9 @@
+import os
+from os import system
+import connection
+from connection import conn
+import colorama
+from colorama import Fore, Style
+from datetime import date
+
+# THE FUNCTIONS FOR THE PROJECT
