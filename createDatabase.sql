351d1c00d3d9be816dfe7d90a8b0d20edbcc9bae
highonchai <connor.high@gmail.com>
2020-11-09 16:45:41 -0500

    updated database queries

--- createDatabase.sql
+++ createDatabase.sql
@@ -41,7 +41,7 @@
 --Purchases Table (INSERT from user input with vscode)
 CREATE TABLE purchases
 (
-purchase_id serial primary key,
+purchase_id int primary key AUTO_INCREMENT,
 user_id varchar(8) not null,
 product_id varchar(8) not null,
 purchase_date date not null,
@@ -54,52 +54,57 @@ foreign key (product_id) references products (product_id) on delete cascade on u
 --Create the Products Table
 CREATE TABLE products
 (
-product_id serial primary key,
+product_id int primary key AUTO_INCREMENT,
 product_name text not null,
-product_color text not null,
+product_price float not null,
 product_description text not null
 )
 
+DROP TABLE products
+
 -- Inserts items into the products table
-INSERT INTO products (product_name, product_price, product_description) VALUES 
-('College rule notebook', 4.99, 'Five Star College notebook 100 pages'),
-('Oxford ruled index cards',  3.99, '100 pack wide ruled'),
-('Pens', 2.99, 'Fine tip black 2 pack'),
-('Colored Pencils', 1.99, '12 pack'),
-('Notebook Paper', 2.99, '120 page pack wide rule'),
-('Microsoft - Surface Studio 2', 3500, '28 Touch-Screen All-In-One Intel Core i7 16GB 1TB SSD' ),
-('Microsoft - Surface Studio 2', 4500, '28 Touch-Screen All-In-One Intel Core i7 32GB 1TB SSD' ),
-('Apple - 27" iMac Pro', 4100, 'iMac Pro with Retina 5K display 32GB Memory - 1TB SSD - Black' ),
-('Apple - MacBook Pro - 16', 2100, 'Display with Touch Bar Intel Core i7 16GB Memory 512GB SSD'),
-('Grapic Tee', 24.99, 'Uni-sexed XL'),
-('Sweatshirt', 26.99, 'Uni-sexed XL'),
-('Logo Socks', 5.99, 'Uni-sexed XL'),
-('Grapic Tee', 24.99, 'Uni-sexed Large'),
-('Sweatshirt', 26.99, 'Uni-sexed Large'),
-('Logo Socks', 5.99, 'Uni-sexed Large'),
-('Grapic Tee', 24.99, 'Uni-sexed Medium'),
-('Sweatshirt', 26.99, 'Uni-sexed Medium'),
-('Logo Socks', 5.99, 'Uni-sexed Medium'),
-('Grapic Tee', 24.99, 'Uni-sexed Small'),
-('Sweatshirt', 26.99, 'Uni-sexed Small'),
-('Logo Socks', 5.99 , 'Uni-sexed Small')
+INSERT INTO products (product_name, product_price, product_description) VALUES
+("College rule notebook", 4.99, "Five Star College notebook 100 pages"),
+("Oxford ruled index cards",  3.99, "100 pack wide ruled"),
+("Pens", 2.99, "Fine tip black 2 pack"),
+("Colored Pencils", 1.99, "12 pack"),
+("Notebook Paper", 2.99, "120 page pack wide rule"),
+("Microsoft - Surface Studio 2", 3500.00, "28 Touch-Screen All-In-One Intel Core i7 16GB 1TB SSD"),
+("Microsoft - Surface Studio 2", 4500.00, "28 Touch-Screen All-In-One Intel Core i7 32GB 1TB SSD"),
+("Apple - iMac Pro - 27", 4100.00, "iMac Pro with Retina 5K display 32GB Memory - 1TB SSD - Black"),
+("Apple - MacBook Pro - 16", 2100.00, "Display with Touch Bar Intel Core i7 16GB Memory 512GB SSD"),
+("Grapic Tee", 24.99, "Uni-sexed XL"),
+("Sweatshirt", 26.99, "Uni-sexed XL"),
+("Logo Socks", 5.99, "Uni-sexed XL"),
+("Grapic Tee", 24.99, "Uni-sexed Large"),
+("Sweatshirt", 26.99, "Uni-sexed Large"),
+("Logo Socks", 5.99, "Uni-sexed Large"),
+("Grapic Tee", 24.99, "Uni-sexed Medium"),
+("Sweatshirt", 26.99, "Uni-sexed Medium"),
+("Logo Socks", 5.99, "Uni-sexed Medium"),
+("Grapic Tee", 24.99, "Uni-sexed Small"),
+("Sweatshirt", 26.99, "Uni-sexed Small"),
+("Logo Socks", 5.99, "Uni-sexed Small")
 
 
 -- Selects ALL from products Table
 SELECT * FROM products
 
---Create users table
+-- Create users table
 CREATE TABLE users
 (
-user_id serial primary key,
-user_firstame text not null,
+user_id int primary key AUTO_INCREMENT,
+user_firstname text not null,
 user_lastname text not null,
 user_phone text not null,
 user_email text not null,
 user_password text not null,
-user_role text not null, --Teacher, Student, Admin, etc
+user_role text not null,
 user_department text not null
 )
+--Teacher, Student, Admin, etc
+
+drop table users
 
 -- Insert users into users table
 INSERT INTO users (user_firstname, user_lastname, user_phone, user_email, user_password, user_role, user_department) VALUES
@@ -107,28 +112,40 @@ INSERT INTO users (user_firstname, user_lastname, user_phone, user_email, user_p
 ("Emily", "Gold", "222-222-1234", "emily.gold@gmail.com", "passTeacher", "Teacher", "Staff"),
 ("Violet", "Waters", "123-123-1234", "violet.waters@gmail.com", "passTeacher", "Teacher", "Staff"),
 ("Trevor", "Stump", "111-111-1234", "trevor.stump@gmail.com", "passStudent", "Student", "Student"),
-("Hannah", "Stump", "110-110-1234", "hannah.stump@gmail.com", "passStudent", "Student", "Student")
+("Hannah", "Stump", "110-110-1234", "hannah.stump@gmail.com", "passStudent", "Student", "Student"),
+("Becca", "Smith", "112-112-1234", "becca.smith@gmail.com", "passStudent", "Student", "Student"),
+("John", "Smith", "113-113-1234", "john.smith@gmail.com", "passStudent", "Student", "Student")
 
 --Enrollment Table ()
 CREATE TABLE enrollments
 (
-enrollment_id serial primary key,
+enrollment_id int primary key AUTO_INCREMENT,
 user_id int not null,
 course_id int not null,
-enrollment_grade decimal(3,2)
-foreign key (user_id) references users (user_id) on delete cascade on update cascade,
-foreign key (class_id) references classes (class_id) on delete cascade on update cascade
+enrollment_grade decimal(3,2),
+foreign key (user_id) references users(user_id) on delete cascade,
+foreign key (course_id) references courses(course_id) on delete cascade
 )
+
+INSERT INTO enrollments (user_id, course_id, enrollment_grade) VALUES
+(4, 2, 90),
+(5, 1, 84),
+(6, 3, 77),
+(4, 1, 92),
+(5, 3, 82)
 
 --Class Table
 CREATE TABLE courses
 (
-course_id serial primary key,
+course_id int primary key AUTO_INCREMENT,
 course_name text not null,
 course_size int not null,
-course_instructor text not null
+course_instructorid int not null,
+foreign key (course_instructorid) references users(user_id) on delete cascade
 )
 
 INSERT INTO courses (course_name, course_size, course_instructor) VALUES
-()
+("History", 15, 2),
+("Math", 15, 3),
+("Science", 15, 3)
 
