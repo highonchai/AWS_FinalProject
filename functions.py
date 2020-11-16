47197dda4d3c7e279a69b1475f1a8fef125fe2a5
highonchai <connor.high@gmail.com>
2020-11-16 12:19:10 -0500

    Created function to show enrollments by student

--- functions.py
+++ functions.py
@@ -49,5 +49,46 @@ def displayAllEnrollmentsTable():
     except(Exception, mysql.connector.Error) as error:
         print('Error while fetching data from database', error)
 
+# displayAllEnrollmentsTable()
 
-displayAllEnrollmentsTable()
+
+def displayEnrollmentsByStudentTable(student):
+    system('clear')
+    tableName1 = 'enrollments'
+    tableName2 = 'users'
+    tableName3 = 'courses'
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            f'''
+            SELECT e.enrollment_id as 'Enrollment ID', 
+            CONCAT(u.user_firstname, ' ', u.user_lastname) as 'Student Name', 
+            e.user_id as 'Student ID', 
+            c.course_name as 'Course Name', 
+            c.course_id as 'Course ID',
+            e.enrollment_grade as 'Student Grade' 
+            FROM {tableName1} e
+            LEFT JOIN {tableName2} u on u.user_id = e.user_id
+            LEFT JOIN {tableName3} c on c.course_id = e.course_id
+            WHERE u.user_id = {student}
+            ''')
+        print(Style.RESET_ALL)
+        print(Fore.GREEN, 'ENROLLMENT INFO: ')
+        print(Style.RESET_ALL)
+        for row in cursor:
+            print(f'''{Fore.YELLOW}
+            Enrollment ID: .....................{Style.RESET_ALL}{Fore.CYAN}{row[0]}{Fore.YELLOW}
+            Course Name: .......................{Style.RESET_ALL}{Fore.CYAN}{row[3]}{Fore.YELLOW}
+            Course ID: .........................{Style.RESET_ALL}{Fore.CYAN}{row[4]}{Fore.YELLOW}
+            Student Name: ......................{Style.RESET_ALL}{Fore.CYAN}{row[1]}{Fore.YELLOW}
+            Student ID: ........................{Style.RESET_ALL}{Fore.CYAN}{row[2]}{Fore.YELLOW}
+            Student Grade: .....................{Style.RESET_ALL}{Fore.CYAN}{row[5]}{Fore.YELLOW}
+            ''')
+        cursor.close()
+        conn.close()
+
+    except(Exception, mysql.connector.Error) as error:
+        print('Error while fetching data from database', error)
+
+
+displayEnrollmentsByStudentTable(4)
